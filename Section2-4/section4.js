5d3f3e0e59b7187c58498e7e6c1cece881dcdc97
royanlord <royanfarid32@gmail.com>
2023-04-06 13:02:01 +0700

    update section 4

--- Section2-4/section4.js
+++ Section2-4/section4.js
@@ -1,7 +1,7 @@
 let btnCreate = document.getElementById('btn');
 btnCreate.addEventListener('click', function() {
-    let div = document.createElement('div');
-    div.id = "box"
-    div.innerHTML = "<p>Hello World!</p>"
-    document.body.appendChild(div)
+    let div = document.createElement('div'); // membuat element div
+    div.id = "box" // menambahkan id pada div tsb
+    div.innerHTML = "<p>Hello World!</p>" 
+    document.body.appendChild(div) // menambahkan element p ke dalam div dengan id box
 })
