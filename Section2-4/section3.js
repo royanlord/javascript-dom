c43fc3e7113eb55b7400436d4be200da63aef995
royanlord <royanfarid32@gmail.com>
2023-04-06 12:39:26 +0700

    update section 3

--- Section2-4/section3.js
+++ Section2-4/section3.js
@@ -1,2 +1,6 @@
-let x = document.getElementById("myP").parentElement.nodeName;
-document.getElementById("demo").innerHTML = x;
+let menu = document.getElementById('menu');
+// console.log(menu.lastElementChild);
+let first = menu.firstElementChild;
+let last = menu.lastElementChild
+first.style.color = "red" // merubah color text pada child pertama
+last.style.backgroundColor = "aqua" // merubah bg color pada child terakhir
